0fc930ea16bc288d44fe11d4f650d0582ff82a8a
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2019-02-05 22:11:21 -0500

    Changed Javascript to Swift

---
+++ algorytm/Swift/algorytm.swift
@@ -0,0 +1,3 @@
+// Napiś tutaj algorytm w języku Swift, który napisałeś w pseudocode
+// Używaj Google aby się dowiedzieć syntax
+// Aby potwierdzić, że twój algorytm działa dobrze, sprawdź go używając test cases
