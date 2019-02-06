1b51fcefc91af3f493af5a94a9c0cfbf363417e3
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2019-02-05 22:12:43 -0500

    Got rid of Javascript

--- algorytm/Javascript/algorytm.js
+++
@@ -1,3 +0,0 @@
-// Napiś tutaj algorytm w języku Javascript, który napisałeś w pseudocode
-// Używaj Google aby się dowiedzieć syntax
-// Aby potwierdzić, że twój algorytm działa poprawnie, sprawdź go używając test cases
